7aad2b689103041f020648ed05b77098f769a2dc
Masurun <wataru.5756@gmail.com>
2021-03-10 15:03:04 +0900

    userモデルの単体テストコードの実装

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :post do
-    
+    spa_name              {'test'}
+    congestion_rate_id    {2}
+    visit_time_id         {2}
+    water_temp_id         {2}
+    sauna_temp_id         {2}
+    pref_id               {2}
+    chair_count_id        {2}
+    association :user
+    after(:build) do |post|
+      post.image.attach(io: File.open('public/images/test_image.png'),filename: 'test_image.png')
   end
+end
 end
